8ea6b25a843ee95c2908a806f9d4ba2c73bc516f
BryanCondori23 <harolcondori-57@gmail.com>
2024-05-07 12:17:02 -0400

    endpoint tarea

--- index.js
+++ index.js
@@ -6,19 +6,33 @@ const PORT = 3000; // PUEDE CAMBIAR DEPENDIENDO QUE PUERTO TRABAJEMOS ES NECESAR
 // creamos un array o un listado de libros de
 
 let librosBiblicos = [
-    { id: 1 , nombre: 'Genesis', autor: 'Moises'},
-    { id: 2 , nombre: 'Exodo', autor: 'Moises'},
-    { id: 3 , nombre: 'Levitico', autor: 'Moises'},
+    { id: 1 , nombre: 'Genesis', autor: 'Moises', anioPublicacion: '1000'},
+    { id: 2 , nombre: 'Exodo', autor: 'Moises', anioPublicacion: '2000'},
+    { id: 3 , nombre: 'Levitico', autor: 'Moises', anioPublicacion:'2024'},
 
 
 
 ];
+
+let nombre = [
+    {nombre: 'Bryan Harol Condori Rafael ', profesion: 'Tecnico Superior Informatica'},
+    
+];
+
+
 //mensaje Json
  app.use(express.json());
  //endpoint 1 OBTENER TODOS LOS LIBROS
  app.get('/libros', (req, res) => {
     res.json(librosBiblicos);
  });
+
+ //endpoint para obtener el nombre y la profesion
+
+ app.get('/nombres', (req, res) => {
+    res.json(nombre);
+ });
+
  //enppint 2 obtener libro por ID
  app.get('/libros/:id',(req, res) => {
    const idCapturado = parseInt(req.params.id);
@@ -30,6 +44,7 @@ let librosBiblicos = [
        res.status(404).json({mensaje : 'Libro no encontrado'});
    }
 });
+
 // endpoint 3 agregar un libro
 app.post('/agregar-libro', (req, res) => {
    const nuevoLibro = req.body;
@@ -37,6 +52,7 @@ app.post('/agregar-libro', (req, res) => {
    librosBiblicos.push(nuevoLibro);
    res.status(201).json('este libro fue guardado exitosamente');
 })
+
 //endpoint 4 actualizar el libro
 app.put('/actualizar-libro/:id', (req, res) => {
    const idCapturado = parseInt(req.params.id);
@@ -47,6 +63,25 @@ app.put('/actualizar-libro/:id', (req, res) => {
    } else {
        res.status(404).json({mensaje : 'Libro no encontrado'});
    }
+});
+
+// endpoint 5 Eliminar Libro
+app.delete('/eliminar-libro/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    lBiblico = librosBiblicos.filter( libro => libro.id !== id);
+    res.status(201).json({mensaje : 'se ha eliminado el libro'});
+    console.log(lBiblico);
+});
+
+//endpoint 6 
+app.get('/libros/publicacion/:anio', (req, res) => {
+    const year =  parseInt(req.params.anio);
+    const librosPublicados = librosBiblicos.filter( x => x.anioPublicacion === year);
+    if (librosPublicados.length > 0) {
+        res.json(librosPublicados);
+    } else {
+        res.status(404).json({mensaje : 'no se han encontrado libros publicados en ese anio'});
+    }
 });
 
 
